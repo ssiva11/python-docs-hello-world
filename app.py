fafb251d243e617642773e88d3c4160fb2ad5676
ssiva11 <39625540+ssiva11@users.noreply.github.com>
2021-06-27 13:31:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello, World Welcome to my first app!"
-
-@app.route('/product/<name>')
-def get_product(name):
-  return "The product is " + str(name)
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
+if __name__ == '__main__':
+    app.run()
