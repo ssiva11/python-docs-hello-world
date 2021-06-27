20d1b09a2139204ef7d8f48b12c8272b1f09e0b4
ssiva11 <39625540+ssiva11@users.noreply.github.com>
2021-06-27 12:51:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World Welcome to my first app!"
+
+@app.route('/product/<name>')
+def get_product(name):
+  return "The product is " + str(name)
